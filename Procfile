75cd020c32d61e95f79a2e0af2babc543ec8ba5f
vandalv <vandalv@tut.by>
2021-04-01 00:38:39 +0200

    ready1

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 heroku ps:scale web=1
-web: gunicorn run:app
+web: gunicorn webscrap:app
--- main.py
+++ main.py
@@ -116,5 +116,4 @@ def delete_post(post_id):
 
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True)
